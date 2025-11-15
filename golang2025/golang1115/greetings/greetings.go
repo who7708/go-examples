868f882cab5aa6a12fffc633303b2c20cde18429
Chris <who7708@163.com>
2025-11-15 23:16:28 +0800

    返回一个随机的问候语

--- golang2025/golang1115/greetings/greetings.go
+++ golang2025/golang1115/greetings/greetings.go
@@ -1,12 +1,38 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// init 为函数中使用的变量设置初始值
+// init 函数会在包被引用时自动调用
+func init() {
+	fmt.Println("greetings package initialized")
+	// rand.Seed(time.Now().UnixNano())
+	rand.NewSource(time.Now().UnixNano())
+}
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// 如果 name 为空字符串，返回错误
+	if name == "" {
+		return "", errors.New("姓名不能为空")
+	}
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
 
 // 在 Go 中，名称以大写字母开头的函数可以由不在同一包中的函数调用。这在 Go 中称为导出的名称
