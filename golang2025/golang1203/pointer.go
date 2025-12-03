9011c0bf641124afc515c03a10f860902755bb2a
Chris <who7708@163.com>
2025-12-03 23:56:02 +0800

    指针

---
+++ golang2025/golang1203/pointer.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	main1()
+	fmt.Printf("main1: %v\n", main1)
+	main2()
+	fmt.Printf("main2: %v\n", main2)
+	main3()
+	fmt.Printf("main3: %v\n", main3)
+}
+
+func main1() {
+	var a int = 10
+
+	fmt.Printf("变量的地址: %x\n", &a)
+}
+
+func main2() {
+	var a int = 20 /* 声明实际变量 */
+	var ip *int    /* 声明指针变量 */
+
+	ip = &a /* 指针变量的存储地址 */
+
+	fmt.Printf("a 变量的地址是: %x\n", &a)
+
+	/* 指针变量的存储地址 */
+	fmt.Printf("ip 变量储存的指针地址: %x\n", ip)
+
+	/* 使用指针访问值 */
+	fmt.Printf("*ip 变量的值: %d\n", *ip)
+}
+
+func main3() {
+
+	var a int
+	var ptr *int
+	var pptr **int
+
+	a = 3000
+
+	/* 指针 ptr 地址 */
+	ptr = &a
+
+	/* 指向指针 ptr 地址 */
+	pptr = &ptr
+
+	/* 获取 pptr 的值 */
+	fmt.Printf("变量 a = %d\n", a)
+	fmt.Printf("指针变量 *ptr = %d\n", *ptr)
+	fmt.Printf("指向指针的指针变量 **pptr = %d\n", **pptr)
+}
